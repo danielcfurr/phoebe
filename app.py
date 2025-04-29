831c5ac371d7451d2d932b463a54fe4a01a50410
Daniel Furr <danielcfurr@gmail.com>
2025-04-28 17:08:52 -0700

    Bring bird images into the app

--- app.py
+++ app.py
@@ -6,14 +6,20 @@ import argparse
 from time import time
 
 
-AUDIO_PATH = Path().resolve() / "audio" / "data" / "processed"
+AUDIO_DIR = Path().resolve() / "audio" / "data" / "processed"
 APP_DATA_PATH = Path().resolve() / "audio" / "data" / "app_data.json"
 RATINGS_LOG_PATH = Path().resolve() / "ratings.jsonl"
+BIRD_IMAGE_DATA_PATH = Path().resolve() / "images" / "data" / "bird_images.jsonl"
+BIRD_IMAGE_DIR = Path().resolve() / "images" / "data" / "birds"
 
 with open(APP_DATA_PATH) as file:
     APP_DATA = json.load(file)
 
 BIRDS = list(APP_DATA.keys())
+
+with open(BIRD_IMAGE_DATA_PATH, "r") as file:
+    BIRD_IMAGE_DATA = [json.loads(line) for line in file]
+    BIRD_IMAGE_DATA = {x["scientific_name"]: x for x in BIRD_IMAGE_DATA}
 
 
 def get_args():
@@ -39,7 +45,7 @@ def generate_item():
 
 
 def audio_widget(record):
-    file_path = AUDIO_PATH / record["file_name"]
+    file_path = AUDIO_DIR / record["file_name"]
     audio_file = open(file_path, "rb")
     audio_bytes = audio_file.read()
     st.audio(audio_bytes, format="audio/mp3")
@@ -47,17 +53,27 @@ def audio_widget(record):
 
 def credit(record, reveal=True):
     if reveal:
-        lic_text, lic_number = PurePosixPath(record["license"]).parts[-2:]
+        recording_lic_text, recording_lic_number = PurePosixPath(record["license"]).parts[-2:]
+        scientific_name = record["scientific_name"]
+
+        image = BIRD_IMAGE_DATA[scientific_name]
+        image_lic_text, image_lic_number = PurePosixPath(image["license_url"]).parts[-2:][:2]
+
         st.write(f"""
-            **{record['common_name']}** <br>
+            **{record["common_name"]}**
+            (*{scientific_name}*) <br>
             <span style="color:gray;">
             [Recording]({record["url"]}): 
             {record["author"]} 
-            ([CC {lic_text} {lic_number}]({record["license"]})) <br>
-            Photo: First Last (License) <br>
+            ([CC {recording_lic_text} {recording_lic_number}]({record["license"]})) <br>
+            [Photo]({image["url"]}): 
+            {image["author"]} 
+            ([CC {recording_lic_text} {recording_lic_number}]({image["license_url"]}))
             </span>
         """, unsafe_allow_html=True)
+
     else:
+
         st.write("""
             *Secret bird* <br>
             <span style="color:gray;">
@@ -122,9 +138,9 @@ for i, rec in enumerate(records):
 
         with image_col:
             if st.session_state.submitted:
-                st.image("temp_bird.jpg", use_container_width=True)
+                st.image(BIRD_IMAGE_DIR / (rec["scientific_name"] + ".jpg"), use_container_width=False)
             else:
-                pass
+                st.markdown("<h1 style='text-align: center; color: gray;'>?</h1>", unsafe_allow_html=True)
 
     if args.rate:
         with st.container():
