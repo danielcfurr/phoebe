bebb470d444796628d39b60a7aff4e88d2f20d75
Daniel Furr <danielcfurr@gmail.com>
2025-04-29 15:08:03 -0700

    Refactor streamlit app

--- app.py
+++ app.py
@@ -7,94 +7,210 @@ import argparse
 
 AUDIO_DIR = Path().resolve() / "audio" / "data" / "processed"
 APP_DATA_PATH = Path().resolve() / "audio" / "data" / "app_data.json"
-RATINGS_LOG_PATH = Path().resolve() / "ratings.json"
+RATINGS_PATH = Path().resolve() / "ratings.json"
 BIRD_IMAGE_DATA_PATH = Path().resolve() / "images" / "data" / "bird_images.jsonl"
 BIRD_IMAGE_DIR = Path().resolve() / "images" / "data" / "birds"
 
-with open(APP_DATA_PATH) as file:
-    APP_DATA = json.load(file)
 
-BIRDS = list(APP_DATA.keys())
-
-with open(BIRD_IMAGE_DATA_PATH, "r") as file:
-    BIRD_IMAGE_DATA = [json.loads(line) for line in file]
-    BIRD_IMAGE_DATA = {x["scientific_name"]: x for x in BIRD_IMAGE_DATA}
-
-if RATINGS_LOG_PATH.exists():
-    with open(RATINGS_LOG_PATH, "r") as file:
-        ratings = json.load(file)
-else:
-    ratings = {}
-    for bird, records in APP_DATA.items():
-        for record in records:
-            ratings[record["recording_id"]] = {
-                "start_sec": record["start_sec"],
-                "end_sec": record["end_sec"],
-                "Noise": None,
-                "Presence": None,
-                "Multiple": None,
-            }
+@st.cache_resource
+def load_app_data():
+    with open(APP_DATA_PATH) as file:
+        recordings = json.load(file)
+    scientific_names = list(recordings.keys())
+    with open(BIRD_IMAGE_DATA_PATH, "r") as file:
+        images = [json.loads(line) for line in file]
+        images = {img["scientific_name"]: img for img in images}
+    return scientific_names, recordings, images
 
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--rate",
-        action="store_true",
-        help="Enable rating mode"
-    )
-    args, _ = parser.parse_known_args()
-    return args
+SCIENTIFIC_NAMES, RECORDINGS, IMAGES = load_app_data()
 
 
-def generate_item():
-    odd_bird, paired_bird = random.sample(BIRDS, k=2)
-    odd_list = random.sample(APP_DATA[odd_bird], k=1)
-    paired_list = random.sample(APP_DATA[paired_bird], k=2)
-    complete_list = odd_list + paired_list
-    order = random.sample(range(3), k=3)
-    ordered_list = [complete_list[j] for j in order]
-    key = order.index(0)
-    return ordered_list, key
+def main(rate: bool):
+    page = st.session_state.get("page", "start")
+    st.session_state.counter = st.session_state.get("counter", 1)
+
+    if rate:
+        st.session_state.rate = True
+        st.session_state.ratings = st.session_state.get("ratings", load_ratings())
+    else:
+        st.session_state.rate = False
+        st.session_state.ratings = {}
+
+    if page == "start":
+        show_start_page()
+    elif st.session_state.counter > 3:
+        show_conclusion_page()
+    elif page == "question":
+        show_question_page()
+    elif page == "review":
+        show_review_page()
 
 
-def audio_widget(record):
-    file_path = AUDIO_DIR / record["file_name"]
+def show_start_page():
+    st.markdown("# Start")
+
+    if st.button("Start", key=f"btn_start"):
+        st.session_state.page = "question"
+        generate_question()
+        st.rerun()
+
+
+def show_question_page():
+    st.markdown(f"# Question {st.session_state.counter}")
+
+    opt_1 = render_option(0, st.session_state.recordings[0], active=True)
+    if st.session_state.rate:
+        render_ratings(st.session_state.recordings[0])
+
+    opt_2 = render_option(1, st.session_state.recordings[1], active=True)
+    if st.session_state.rate:
+        render_ratings(st.session_state.recordings[1])
+
+    opt_3 = render_option(2, st.session_state.recordings[2], active=True)
+    if st.session_state.rate:
+        render_ratings(st.session_state.recordings[2])
+
+    x = [opt_1, opt_2, opt_3]
+
+    if any(x):
+        st.session_state.answer = x.index(True)
+        st.session_state.page = "review"
+        st.rerun()
+
+
+
+def show_review_page():
+    st.markdown(f"# Review {st.session_state.counter}")
+
+    _ = render_option(0, st.session_state.recordings[0], active=False)
+    if st.session_state.rate:
+        render_ratings(st.session_state.recordings[0])
+
+    _ = render_option(1, st.session_state.recordings[1], active=False)
+    if st.session_state.rate:
+        render_ratings(st.session_state.recordings[1])
+
+    _ = render_option(2, st.session_state.recordings[2], active=False)
+    if st.session_state.rate:
+        render_ratings(st.session_state.recordings[2])
+
+    if st.session_state.answer == st.session_state.key:
+        st.success("✅ Correct!")
+    else:
+        st.error(f"❌ Incorrect.")
+
+    if st.button("Next", key=f"btn_next"):
+        if st.session_state.rate:
+            save_ratings()
+        st.session_state.page = "question"
+        st.session_state.counter += 1
+        generate_question()
+        st.rerun()
+
+
+def show_conclusion_page():
+    st.markdown("# Conclusion")
+
+    if st.button("Replay", key=f"btn_replay"):
+        st.session_state.page = "start"
+        st.session_state.counter = 1
+        st.rerun()
+
+
+def load_ratings():
+    if RATINGS_PATH.exists():
+        with open(RATINGS_PATH, "r") as file:
+            ratings = json.load(file)
+    else:
+        ratings = {}
+        for bird, records in RECORDINGS.items():
+            for record in records:
+                ratings[record["recording_id"]] = {
+                    "start_sec": record["start_sec"],
+                    "end_sec": record["end_sec"],
+                    "Noise": None,
+                    "Presence": None,
+                    "Multiple": None,
+                }
+    return ratings
+
+
+def save_ratings():
+    with open(RATINGS_PATH, "w") as file:
+        json.dump(st.session_state.ratings, fp=file, indent=4)
+
+
+def audio_widget(recording):
+    file_path = AUDIO_DIR / recording["file_name"]
     audio_file = open(file_path, "rb")
     audio_bytes = audio_file.read()
     st.audio(audio_bytes, format="audio/mp3")
 
 
-def credit(record, reveal=True):
-    if reveal:
-        recording_lic_text, recording_lic_number = PurePosixPath(record["license"]).parts[-2:]
-        scientific_name = record["scientific_name"]
+def generate_question():
+    odd_bird, paired_bird = random.sample(SCIENTIFIC_NAMES, k=2)
+    odd_list = random.sample(RECORDINGS[odd_bird], k=1)
+    paired_list = random.sample(RECORDINGS[paired_bird], k=2)
+    complete_list = odd_list + paired_list
+    order = random.sample(range(3), k=3)
 
-        image = BIRD_IMAGE_DATA[scientific_name]
-        image_lic_text, image_lic_number = PurePosixPath(image["license_url"]).parts[-3:-1]
+    st.session_state.key = order.index(0)
+    st.session_state.recordings = [complete_list[j] for j in order]
+    st.session_state.images = [IMAGES[rec["scientific_name"]] for rec in st.session_state.recordings]
 
-        st.write(f"""
-            **{record["common_name"]}**
-            (*{scientific_name}*) <br>
-            <span style="color:gray;">
-            [Audio]({record["url"]}): 
-            {record["author"]} 
-            ([CC {recording_lic_text} {recording_lic_number}]({record["license"]})) <br>
-            [Photo]({image["url"]}): 
-            {image["author"]} 
-            ([CC {image_lic_text} {image_lic_number}]({image["license_url"]}))
-            </span>
-        """, unsafe_allow_html=True)
 
-    else:
+def render_option(i, recording, active=True):
+    with st.container():
+        button_col, recording_col, image_col = st.columns([.5, 4, 1])
 
-        st.write("""
-            *Secret bird* <br>
-            <span style="color:gray;">
-            Audio: *???* <br>
-            Photo: *???* <br>
-            </span>
-        """, unsafe_allow_html=True)
+        with button_col:
+            if active:
+                selected = st.button("ABC"[i], key=f"btn_{i}", )
+            elif st.session_state.key == i:
+                st.button("✅", key=f"btn_{i}", disabled=True)
+                selected = None
+            elif st.session_state.answer == i:
+                st.button("❌", key=f"btn_{i}", disabled=True)
+                selected = None
+            else:
+                selected = None
+
+        with recording_col:
+            audio_widget(recording)
+            if active:
+                st.write("""
+                    *Secret bird* <br>
+                    <span style="color:gray;">
+                    Audio: *???* <br>
+                    Photo: *???* <br>
+                    </span>
+                """, unsafe_allow_html=True)
+            else:
+                recording_lic_text, recording_lic_number = PurePosixPath(recording["license"]).parts[-2:]
+                scientific_name = recording["scientific_name"]
+                image = IMAGES[scientific_name]
+                image_lic_text, image_lic_number = PurePosixPath(image["license_url"]).parts[-3:-1]
+                st.write(f"""
+                    **{recording["common_name"]}**
+                    (*{scientific_name}*) <br>
+                    <span style="color:gray;">
+                    [Audio]({recording["url"]}): 
+                    {recording["author"]} 
+                    ([CC {recording_lic_text} {recording_lic_number}]({recording["license"]})) <br>
+                    [Photo]({image["url"]}): 
+                    {image["author"]} 
+                    ([CC {image_lic_text} {image_lic_number}]({image["license_url"]}))
+                    </span>
+                """, unsafe_allow_html=True)
+
+        with image_col:
+            if active:
+                st.markdown("<h1 style='text-align: center; color: gray;'>?</h1>", unsafe_allow_html=True)
+            else:
+                st.image(BIRD_IMAGE_DIR / (recording["scientific_name"] + ".jpg"), use_container_width=False)
+
+    return selected
 
 
 def rating_widget(record_id, label, options):
@@ -103,98 +219,35 @@ def rating_widget(record_id, label, options):
     return st.radio(label + ":", options=options, key=f"radio_{label}_{record_id}", horizontal=False, index=idx)
 
 
-def blank_ratings():
-    ratings = [{"Noise": None, "Presence": None, "Multiple": None} for _ in range(3)]
-    return ratings
-
-
-# Data set up
-
-args = get_args()
-
-# Set up session state
-if "records" not in st.session_state:
-    st.session_state.records, st.session_state.answer_key = generate_item()
-    st.session_state.recording_ids = [rec["recording_id"] for rec in st.session_state.records]
-    st.session_state.ratings = {rid: ratings[rid] for rid in st.session_state.recording_ids}
-if "submitted" not in st.session_state:
-    st.session_state.submitted = False
-if "next_question" not in st.session_state:
-    st.session_state.next_question = False
-if "selected" not in st.session_state:
-    st.session_state.selected = None
-
-
-# Layout starts here...
-
-st.title("Odd Bird Out")
-
-for i, rec in enumerate(st.session_state.records):
-    recording_id = st.session_state.recording_ids[i]
+def render_ratings(recording):
+    recording_id = recording["recording_id"]
     with st.container():
-        button_col, audio_col, image_col = st.columns([.5, 4, 1])
+        _, presence_col, noise_col, multiple_col = st.columns([.5] + [5 / 3] * 3)
 
-        with button_col:
-            if st.session_state.submitted:
-                if i == st.session_state.answer_key:
-                    st.button("✅", key=f"btn_{i}")
-                elif i == st.session_state.selected:
-                    st.button("❌", key=f"btn_{i}")
-            elif st.button("ABC"[i], key=f"btn_{i}"):
-                st.session_state.selected = i
-                st.session_state.submitted = True
-                st.rerun()
+        with presence_col:
+            st.session_state.ratings[recording_id]["Presence"] = rating_widget(
+                recording_id, label="Presence", options=["Full", "Sparse", "Delayed"]
+            )
 
-        with audio_col:
-            audio_widget(rec)
-            credit(rec, st.session_state.submitted)
+        with noise_col:
+            st.session_state.ratings[recording_id]["Noise"] = rating_widget(
+                recording_id, label="Noise", options=["None", "Okay", "Bad"]
+            )
 
-        with image_col:
-            if st.session_state.submitted:
-                st.image(BIRD_IMAGE_DIR / (rec["scientific_name"] + ".jpg"), use_container_width=False)
-            else:
-                st.markdown("<h1 style='text-align: center; color: gray;'>?</h1>", unsafe_allow_html=True)
-
-    if args.rate:
-        with st.container():
-            _, presence_col, noise_col, multiple_col = st.columns([.5] + [5/3]*3)
-
-            with presence_col:
-                st.session_state.ratings[recording_id]["Presence"] = rating_widget(
-                    recording_id, label="Presence",  options=["Full", "Sparse", "Delayed"]
-                )
-
-            with noise_col:
-                st.session_state.ratings[recording_id]["Noise"] = rating_widget(
-                    recording_id, label="Noise",  options=["None", "Okay", "Bad"]
-                )
-
-            with multiple_col:
-                st.session_state.ratings[recording_id]["Multiple"] = rating_widget(
-                    recording_id, label="Multiple",  options=["None", "Okay", "Bad"]
-                )
+        with multiple_col:
+            st.session_state.ratings[recording_id]["Multiple"] = rating_widget(
+                recording_id, label="Multiple", options=["None", "Okay", "Bad"]
+            )
 
 
-# --- Feedback ---
-if st.session_state.submitted:
-    if st.session_state.selected == st.session_state.answer_key:
-        st.success("✅ Correct!")
-    else:
-        st.error(f"❌ Incorrect.")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--rate",
+        action="store_true",
+        help="Enable rating mode"
+    )
 
-# --- Next Question Button ---
-if st.session_state.submitted and st.button("Next Question"):
-    # Optional handling of ratings
-    if args.rate:
-        for recording_id, rating_data in st.session_state.ratings.items():
-            ratings[recording_id] = rating_data
-        with open(RATINGS_LOG_PATH, "w") as f:
-            json.dump(ratings, fp=f, indent=4)
+    args, _ = parser.parse_known_args()
 
-    st.session_state.records, st.session_state.answer_key = generate_item()
-    st.session_state.recording_ids = [rec["recording_id"] for rec in st.session_state.records]
-    st.session_state.ratings = {rid: ratings[rid] for rid in st.session_state.recording_ids}
-    st.session_state.submitted = False
-    st.session_state.selected = None
-
-    st.rerun()
+    main(args.rate)
